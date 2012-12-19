fa0ebe9ec4dbb3ba7ab68e3c87d5325eec0fb2a7
Andreas Kaufmann <4lk4tr43@gmail.com>
2012-12-19 20:56:46 +0100

    corrected random for iOS

--- variable.h
+++ variable.h
@@ -6,14 +6,14 @@ using namespace std;
 
 template <class T> class RandomReal
 {
-	mt19937_64 engine;
-	uniform_real<T> u;
+	mt19937 engine;
+	uniform_real_distribution<T> u;
 
 public:
 	RandomReal(T lower, T upper, unsigned long seed = 0)
 	{
 		set_seed(seed);
-		u = uniform_real<T>(lower, upper);
+		u = uniform_real_distribution<T>(lower, upper);
 	}
 	void set_seed(unsigned long seed) 
 	{
@@ -30,14 +30,14 @@ public:
 
 template <class T> class RandomInteger
 {
-	mt19937_64 engine;
-	uniform_int<T> u;
+	mt19937 engine;
+	uniform_int_distribution<T> u;
 
 public:
 	RandomInteger(T lower, T upper, unsigned long seed = 0)
 	{
 		set_seed(seed);
-		u = uniform_int<T>(lower, upper);
+		u = uniform_int_distribution<T>(lower, upper);
 	}
 	void set_seed(unsigned long seed) 
 	{
