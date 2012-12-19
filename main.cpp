c049a8b766506c849c0b61013382018ea8bfdcf3
Andreas Kaufmann <4lk4tr43@gmail.com>
2012-12-19 18:47:23 +0100

    commit

--- main.cpp
+++ main.cpp
@@ -1,18 +1,4 @@
-#include <iostream>
-#include <thread>
-#include <string>
-#include <exception>
-#include <fstream>
-#include <memory>
-#include <chrono>
-#include <random>
-#include <limits>
-#include <algorithm>
-#include <vector>
-using namespace std;
-
-#include "glm/glm.hpp"
-using namespace glm;
+#include "_working_file_.h"
 
 #ifdef WIN32
     #include "gl/glfw.h"
@@ -22,139 +8,6 @@ using namespace glm;
         #include <crtdbg.h>
     #endif
 #endif
-
-#include "data.h"
-
-class AssetManager
-{
-public:
-	string baseFolder;
-
-	AssetManager(string baseFolder = "")
-	{
-		this->baseFolder = baseFolder;
-	}
-
-	void load_screen(string screenName)
-	{
-		throw std::exception();
-	}
-};
-
-class Asset
-{
-public:
-	enum Type
-	{
-		unknown,
-		data,
-
-		texture,
-		shader,
-		mesh,
-		skeleton,
-		
-		audio,
-		music,
-
-		gui,
-		screen
-	};
-
-	Asset()
-	{
-		type = Type::unknown;
-		priority = 0;
-	}
-
-	Type type;
-	unsigned char priority;
-	string missing_url;
-	string name;
-};
-
-class Root
-{
-public:
-	AssetManager *assetManager;
-	bool seperate_thread, star_exit;
-
-	Root(string baseFolder = "")
-	{
-		assetManager = new AssetManager(baseFolder);
-		star_exit = false;
-	}
-	~Root()
-	{
-		delete assetManager;
-	}
-
-	void run(function<void (Root *root)> loop, function<void (void)> init_loop = nullptr, function<void (void)> release_loop = nullptr, bool seperate_thread = true)
-	{		
-		auto loop_lambda = [&]()
-		{
-			if (init_loop) init_loop();
-			loop(this);
-			if (release_loop) release_loop();
-		};
-
-		if (seperate_thread)
-		{
-			thread render_thread(loop_lambda);
-			render_thread.join();
-		}
-		else
-			loop_lambda();
-	}
-};
-
-/*
-template <class T> class RandomReal
-{
-	mt19937_64 engine;
-    uniform_real<T> u;
-
-public:
-	RandomReal(T lower, T upper, unsigned long seed = 0)
-	{
-		set_seed(seed);
-		u = uniform_real<T>(lower, upper);
-	}
-	void set_seed(unsigned long seed) 
-	{
-		if (seed)
-			engine.seed(seed);
-		else
-			engine.seed((unsigned long)time(nullptr));
-	}
-	void operator>>(T &x)
-	{
-		x = u(engine);
-	}
-};
-template <class T> class RandomInteger
-{
-	mt19937_64 engine;
-	uniform_int<T> u;
-
-public:
-	RandomInteger(T lower, T upper, unsigned long seed = 0)
-	{
-		set_seed(seed);
-		u = uniform_int<T>(lower, upper);
-	}
-	void set_seed(unsigned long seed) 
-	{
-		if (seed)
-			engine.seed(seed);
-		else
-			engine.seed((unsigned long)time(nullptr));
-	}
-	void operator>>(T &x)
-	{
-		x = u(engine);
-	}
-};
 
 void init()
 {
@@ -189,9 +42,8 @@ void loop(Root *root)
 
 	cout << min_fps << endl;
 }
- */
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
 	Asset a;
 	a.name = "this is an asset";
@@ -204,11 +56,12 @@ int main(int argc, char **argv)
 	Asset *y = (Asset *)x;
 	string *z = (string *)&x[sizeof(*y)];
 
-	// make_shared<Root>()->run(loop, init, release, false);
+	make_shared<Root>()->run(loop, init, release, false);
 
 #ifdef _DEBUG
 	cin.get();
 	_CrtDumpMemoryLeaks();
 #endif
+
     return 0;
 }
